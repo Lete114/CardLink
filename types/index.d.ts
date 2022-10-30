9b15631bada583b20215745d1c640b86827c2d11
Lete114 <me@imlete.cn>
2022-10-30 10:10:16 +0800

    chore(release): 1.0.2

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,3 +3,9 @@
  * @param {NodeList} nodes A collection of nodes or a collection of arrays, if it is an array then the array must always contain node element
  */
 export default function cardLink(nodes: NodeList): void;
+/**
+ * parse
+ * @param {String} html String type html
+ * @param {String} link Website address
+ */
+export default function cardLinkParse(html: string, link: string): { title: string; link: string; icon: string };
