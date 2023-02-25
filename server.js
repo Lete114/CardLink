5c6229b467d1fc527f773677d4ed67a4aa51a989
Lete114 <me@imlete.cn>
2023-02-25 16:39:33 +0800

    feat: 移植 Deta Space

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const http = require('http')
 const main = require('./main')
 
-const PORT = process.env.PORT || 6870
+const PORT = parseInt(process.env.PORT) || 6870
 
-module.exports = () => {
-  http.createServer(main).listen(PORT, function () {
-    console.log('Server listening on port', PORT)
-  })
-}
+http.createServer(main).listen(PORT, function () {
+  console.log('Server listening on port', PORT)
+})
